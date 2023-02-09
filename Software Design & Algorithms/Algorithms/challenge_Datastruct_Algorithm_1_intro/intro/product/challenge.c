9cedcdfae7fd4bb19de5a22ffa891a729b2349bc
Nathan Thus <99728206+NathanThus@users.noreply.github.com>
2023-02-09 10:18:04 +0100

    Finished Part 1

--- Software Design & Algorithms/Algorithms/challenge_Datastruct_Algorithm_1_intro/intro/product/challenge.c
+++ Software Design & Algorithms/Algorithms/challenge_Datastruct_Algorithm_1_intro/intro/product/challenge.c
@@ -1,9 +1,32 @@
 #include "challenge.h"
+#include <stddef.h>
+
+#define MAXIMUM_N 100000
 
 int FindSmallestNumberThatIsRepeatedKTimes(
             int* array, int size, int k, int* smallestNumber)
 {
-    return -1;
+    if(array == NULL || smallestNumber == NULL)
+    {
+        return -1;
+    }
+
+    int findSmallestArray[MAXIMUM_N];
+
+    for (size_t i = 0; i < size; i++)
+    {
+        findSmallestArray[array[i]]++;
+    }
+
+    for (size_t i = 0; i < size; i++)
+    {
+        if(findSmallestArray[i] == k)
+        {
+            *smallestNumber = findSmallestArray[i];
+            return 0;
+        }
+    }
+    return 1;
 }
 
 int ComputeDifferenceBetweenMaxAndMinSumOfKElements_0(
