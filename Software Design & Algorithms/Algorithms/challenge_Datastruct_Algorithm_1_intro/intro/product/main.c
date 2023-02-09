b528a49775f18d521bc9cb397ca9f8ce7b6f36e8
Nathan Thus <99728206+NathanThus@users.noreply.github.com>
2023-02-09 10:54:05 +0100

    Update main.c
    Implemented parsing

--- Software Design & Algorithms/Algorithms/challenge_Datastruct_Algorithm_1_intro/intro/product/main.c
+++ Software Design & Algorithms/Algorithms/challenge_Datastruct_Algorithm_1_intro/intro/product/main.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 
 #include "challenge.h"
+#include "parse.h"
 
 int main(int argc, char* argv[])
 {
@@ -13,6 +14,28 @@ int main(int argc, char* argv[])
     }
     char selectedAssignment = atoi(argv[1]);
 
+    int n = 0;
+    if(GetNumberOfValues(&n) != 1)
+    {
+        printf("Something went wrong with parsing N!\n");
+        return -1;
+    }
+
+    int data[n];
+
+    if(GetArray(&data[0],n) != 1)
+    {
+        printf("Something went wrong with parsing the data into the array! \n");
+        return -1;
+    }
+    
+
+    int k = 0;
+    if(GetSearchParameter(&k) != 1)
+    {
+        printf("Something went wrong with parsing the search parameter!\n");
+        return -1;
+    }
     // TODO: parse data from stdin as explained in part 0 in the challenge document
     // (obviously: parsing data is another responsibility than commandline UI stuff,
     //  so parsing data should happen in another file!)
@@ -20,39 +43,40 @@ int main(int argc, char* argv[])
     if (selectedAssignment == 1)
     {
         int smallestNumber = 0;
-        FindSmallestNumberThatIsRepeatedKTimes( /* TODO: fill in parameters */ );
+
+        FindSmallestNumberThatIsRepeatedKTimes(&data[0],n,k,&smallestNumber);
 
         printf("%d\n", smallestNumber);
     }
-    else if (selectedAssignment == 2)
-    {
-        int difference = 0;
-        ComputeDifferenceBetweenMaxAndMinSumOfKElements_0( /* TODO: fill in parameters */ );
+    // else if (selectedAssignment == 2)
+    // {
+    //     int difference = 0;
+    //     ComputeDifferenceBetweenMaxAndMinSumOfKElements_0( /* TODO: fill in parameters */ );
 
-        printf("%d\n", difference);
-    }
-    else if (selectedAssignment == 31)
-    {
-        int difference = 0;
-        ComputeDifferenceBetweenMaxAndMinSumOfKElements_1( /* TODO: fill in parameters */ );
-        printf("%d\n", difference);
-    }
-    else if (selectedAssignment == 32)
-    {
-        int difference = 0;
-        ComputeDifferenceBetweenMaxAndMinSumOfKElements_2( /* TODO: fill in parameters */ );
-        printf("%d\n", difference);
-    }
-    else if (selectedAssignment == 33)
-    {
-        int difference = 0;
-        ComputeDifferenceBetweenMaxAndMinSumOfKElements_3( /* TODO: fill in parameters */ );
-        printf("%d\n", difference);
-    }
-    else
-    {
-        printf("Error: Unknown assignment: %d\n", selectedAssignment);
-    }
+    //     printf("%d\n", difference);
+    // }
+    // else if (selectedAssignment == 31)
+    // {
+    //     int difference = 0;
+    //     ComputeDifferenceBetweenMaxAndMinSumOfKElements_1( /* TODO: fill in parameters */ );
+    //     printf("%d\n", difference);
+    // }
+    // else if (selectedAssignment == 32)
+    // {
+    //     int difference = 0;
+    //     ComputeDifferenceBetweenMaxAndMinSumOfKElements_2( /* TODO: fill in parameters */ );
+    //     printf("%d\n", difference);
+    // }
+    // else if (selectedAssignment == 33)
+    // {
+    //     int difference = 0;
+    //     ComputeDifferenceBetweenMaxAndMinSumOfKElements_3( /* TODO: fill in parameters */ );
+    //     printf("%d\n", difference);
+    // }
+    // else
+    // {
+    //     printf("Error: Unknown assignment: %d\n", selectedAssignment);
+    // }
 
     return 0;
 }
