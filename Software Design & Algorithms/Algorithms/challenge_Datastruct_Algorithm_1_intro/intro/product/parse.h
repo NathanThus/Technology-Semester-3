5a33c7d74b0cad9f77b6e46a9b1fcf84bdf12f78
Nathan Thus <99728206+NathanThus@users.noreply.github.com>
2023-02-09 10:18:33 +0100

    Create parse.h

---
+++ Software Design & Algorithms/Algorithms/challenge_Datastruct_Algorithm_1_intro/intro/product/parse.h
@@ -0,0 +1,9 @@
+#ifndef PARSE_H
+#define PARSE_H
+
+int GetNumberOfValues(int* n, char* argv[]);
+int GetArray(int* array, char* argv[]);
+int GetSearchParameter(int* k, char* argv[]);
+
+
+#endif //PARSE_H
