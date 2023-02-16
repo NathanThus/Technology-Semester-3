124c4a27525596dbd45a6ea65696dd830aad5b0c
Nathan Thus <99728206+NathanThus@users.noreply.github.com>
2023-02-16 13:14:39 +0100

    Implemented Part 2 of the LinkedList Excerise

---
+++ Software Design & Algorithms/Algorithms/challenge_Datastruct_Algorithm_2_Linked_List_vs_Arrays/Linked_Lists_vs_Arrays/product/linked_list.c
@@ -0,0 +1,137 @@
+#include "linked_list.h"
+
+#include <stddef.h>
+
+typedef struct element Element;
+struct element
+{
+    void* Data;
+    Element* Next;
+};
+
+struct linkedList
+{
+    int ObjectSize;
+    Element* Head;
+    Element* LastAccessed;
+};
+
+
+LinkedList* InitializeList(int objectSize)
+{
+    if(objectSize < 1)
+    {
+        return NULL;
+    }
+    LinkedList *this = malloc(sizeof(LinkedList));
+    this->ObjectSize = objectSize;
+    this->Head = NULL;
+    return this;
+}
+
+int AddToListTail(LinkedList* list, void* data)
+{
+    if (data == NULL || list == NULL)
+    {
+        return -1;
+    }
+
+    if (list->Head == NULL)
+    {
+        list->Head = malloc(sizeof(Element));
+        // list->Head->Data = malloc(list->ObjectSize);
+        list->Head->Data = data;
+        list->Head->Next = NULL;
+        return 0;
+    }
+
+    Element* elementPtr = list->Head;
+    while (elementPtr->Next != NULL)
+    {
+        elementPtr = elementPtr->Next;
+    }
+
+    elementPtr->Next = malloc(sizeof(Element));
+    // elementPtr->Next->Data = malloc(list->ObjectSize);
+    elementPtr->Next->Data = data;
+    elementPtr->Next->Next = NULL;
+
+    return 0;
+}
+
+
+int RemoveDataFromList(LinkedList* list, void* data)
+{
+    if (data == NULL || list == NULL)
+    {
+        return -1;
+    }
+    Element* prevPtr = NULL;
+    Element* currentPtr = list->Head;
+
+    while (currentPtr != NULL)
+    {
+        prevPtr = currentPtr;
+        if (currentPtr->Data == data)
+        {
+            Element* nextPtr = currentPtr->Next;
+            // free(currentPtr->Data);
+            prevPtr->Next = nextPtr;
+            free(currentPtr);
+            return 1;
+        }
+        currentPtr = currentPtr->Next;
+    }
+    return 0;
+}
+
+int ClearList(LinkedList* list)
+{
+    if (list == NULL)
+    {
+        return -1;
+    }
+
+    Element* elementPtr = list->Head;
+    Element* removerPtr;
+    while (elementPtr->Next != NULL)
+    {
+        removerPtr = elementPtr;
+        elementPtr = elementPtr->Next; // INVALID READ
+        // free(removerPtr->Data);
+        free(removerPtr);
+    }
+    return 0;
+}
+
+int DestructList(LinkedList** list)
+{
+    if(list == NULL)
+    {
+        return -1;
+    }
+    ClearList(*list);
+    free(*list);
+    *list = NULL;
+    list = NULL;
+    return 0;
+}
+
+void* GetHead(LinkedList* list)
+{
+    if(list == NULL)
+    {
+        return NULL;
+    }
+    return list->Head->Data;
+}
+
+void* GetNext(LinkedList* list)
+{
+    if(list->LastAccessed == NULL)
+    {
+        return NULL;
+    }
+    list->LastAccessed = list->LastAccessed->Next;
+    return list->LastAccessed;
+}
---
+++ Software Design & Algorithms/Algorithms/challenge_Datastruct_Algorithm_2_Linked_List_vs_Arrays/Linked_Lists_vs_Arrays/product/linked_list.h
@@ -0,0 +1,32 @@
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
+typedef struct linkedList LinkedList;
+
+// Returns a pointer to a new linked list
+// Returns NULL if objectSize is less than 1
+LinkedList* InitializeList(int objectSize);
+
+// Returns a 0 if the list is sucessfully destroyed
+// Returns a -1 if the list is NULL
+int DestructList(LinkedList** list);
+
+// Returns a 0 if the data is sucessfully added to the list
+// Returns a -1 if the data is NULL or the list is NULL
+int AddToListTail(LinkedList* list, void* data);
+
+// Returns a 0 if the data is sucessfully removed from the list
+// Returns a -1 if the data is NULL or the list is NULL
+int RemoveDataFromList(LinkedList* list, void* data);
+
+// Returns a 0 if the list is cleared
+// Returns a -1 if the list is NULL
+int ClearList(LinkedList* list);
+
+// Returns a pointer to the data at the head of the list
+// Returns NULL if the list is NULL or empty
+void* GetHead(LinkedList* list);
+
+// Returns a pointer to the next element in the list
+// Returns NULL if the list is NULL or empty
+void* GetNext(LinkedList* list);
+#endif
