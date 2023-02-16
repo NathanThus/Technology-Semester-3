545a4530ce1d0e6e76a06fd2ba9920599371fb3c
Nathan Thus <99728206+NathanThus@users.noreply.github.com>
2023-02-16 10:20:07 +0100

    Update challenge.c
    Implemented Gnome, Bubble & Insertion Sort

--- Software Design & Algorithms/Algorithms/challenge_Datastruct_Algorithm_1_intro/intro/product/challenge.c
+++ Software Design & Algorithms/Algorithms/challenge_Datastruct_Algorithm_1_intro/intro/product/challenge.c
@@ -81,17 +81,120 @@ int ComputeDifferenceBetweenMaxAndMinSumOfKElements_0(
 int ComputeDifferenceBetweenMaxAndMinSumOfKElements_1(
             int* array, int size, int k, int* difference)
 {
-    return -1;
+    if(array == NULL || difference == NULL)
+    {
+        return -1;
+    }
+    // This is Bubble Sort
+
+    for (size_t i = 0; i < size - 1; i++)
+    {
+        for (size_t j = 0; j < size - i - 1; j++)
+        {
+            if(array[j] > array[j + 1])
+            {
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+    
+
+    int smallest = 0, largest = 0;
+
+    for (size_t i = 0; i < k; i++)
+    {
+        smallest += array[i];
+    }
+    for (size_t i = size - k; i < size; i++)
+    {
+        largest += array[i];
+    }
+    *difference = largest - smallest;
+
+    return 1;
 }
 
 int ComputeDifferenceBetweenMaxAndMinSumOfKElements_2(
             int* array, int size, int k, int* difference)
 {
-    return -1;
+    if(array == NULL || difference == NULL)
+    {
+        return -1;
+    }
+
+    int key;
+    // This is Insertion Sort
+    for (size_t i = 1; i < size; i++)
+    {
+        key = array[i];
+        int j = i - 1;
+
+        while (j >= 0 && array[j] > key)
+        {
+            array[j + 1] = array[j];
+            j--;
+        }
+        array[j + 1] = key;
+    }
+
+    int smallest = 0, largest = 0;
+
+    for (size_t i = 0; i < k; i++)
+    {
+        smallest += array[i];
+    }
+    for (size_t i = size - k; i < size; i++)
+    {
+        largest += array[i];
+    }
+    *difference = largest - smallest;
+
+    return 1;
 }
 
 int ComputeDifferenceBetweenMaxAndMinSumOfKElements_3(
             int* array, int size, int k, int* difference)
 {
-    return -1;
+    if(array == NULL || difference == NULL)
+    {
+        return -1;
+    }
+
+    // Gnome Sort
+    int index = 0;
+
+    while (index < size)
+    {
+        if (index == 0)
+        {
+            index++;
+        }
+        if (array[index] >= array[index - 1])
+        {
+            index++;
+        }
+        else
+        {
+            int temp = array[index];
+            array[index] = array[index - 1];
+            array[index - 1] = temp;
+            index--;
+        }
+    }
+
+    int smallest = 0, largest = 0;
+
+    for (size_t i = 0; i < k; i++)
+    {
+        smallest += array[i];
+    }
+    for (size_t i = size - k; i < size; i++)
+    {
+        largest += array[i];
+    }
+    *difference = largest - smallest;
+
+    return 1;
 }
