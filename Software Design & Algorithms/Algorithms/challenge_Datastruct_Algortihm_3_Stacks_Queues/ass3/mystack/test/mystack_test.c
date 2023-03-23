5a3e189ac8df33ff7d815e3ae4c03e34f807bacf
Nathan Thus <99728206+NathanThus@users.noreply.github.com>
2023-03-23 16:43:26 +0100

    Update mystack_test.c
    Wrote a ton of tests

--- Software Design & Algorithms/Algorithms/challenge_Datastruct_Algortihm_3_Stacks_Queues/ass3/mystack/test/mystack_test.c
+++ Software Design & Algorithms/Algorithms/challenge_Datastruct_Algortihm_3_Stacks_Queues/ass3/mystack/test/mystack_test.c
@@ -17,12 +17,112 @@ void tearDown(void)
     // This is run after EACH test
 }
 
-void run_my_test()
+void test_myStack_Create_Valid()
 {
-	printf("Please specify your tests\n");
+	StackMeta_t *stack = mystack_create(1);
+	TEST_ASSERT_NOT_NULL(stack);
+	TEST_ASSERT_EQUAL_INT(0, mystack_nofelem(stack));
+	TEST_ASSERT_EQUAL_INT(1, stack->objsize);
+	mystack_destroy(stack);
 }
+
+void test_myStack_Create_InvalidObjectSize_Zero()
+{
+	StackMeta_t *stack = mystack_create(0);
+	TEST_ASSERT_NULL(stack);
+}
+
+void test_myStack_push_InvalidStack()
+{
+	StackMeta_t *stack = NULL;
+	int result = mystack_push(stack, NULL);
+	TEST_ASSERT_EQUAL_INT(-1, result);
+}
+
+void test_myStack_push_InvalidObject()
+{
+	StackMeta_t *stack = mystack_create(1);
+	int result = mystack_push(stack, NULL);
+	TEST_ASSERT_EQUAL_INT(-1, result);
+	mystack_destroy(stack);
+}
+
+void test_myStack_push_Valid()
+{
+	StackMeta_t *stack = mystack_create(sizeof(int));
+	int data = 1;
+	int result = mystack_push(stack, &data);
+	TEST_ASSERT_EQUAL_INT(0, result);
+	TEST_ASSERT_EQUAL_INT(1, mystack_nofelem(stack));
+	TEST_ASSERT_NOT_NULL(stack->stack);
+	mystack_destroy(stack);
+}
+
+void test_myStack_pop_InvalidStack()
+{
+	StackMeta_t *stack = NULL;
+	int result = mystack_pop(stack, NULL);
+	TEST_ASSERT_EQUAL_INT(-1, result);
+}
+
+void test_myStack_pop_NoElements()
+{
+	StackMeta_t *stack = mystack_create(1);
+	int result = mystack_pop(stack, NULL);
+	TEST_ASSERT_EQUAL_INT(-1, result);
+	mystack_destroy(stack);
+}
+
+void test_myStack_pop_Valid()
+{
+	StackMeta_t *stack = mystack_create(sizeof(int));
+	int data = 1;
+	int result = mystack_push(stack, &data);
+	TEST_ASSERT_EQUAL_INT(0, result);
+	TEST_ASSERT_EQUAL_INT(1, mystack_nofelem(stack)); // There is data!
+	data = 0;
+	result = mystack_pop(stack, &data);
+	TEST_ASSERT_EQUAL_INT(0, result);
+	TEST_ASSERT_EQUAL_INT(1, data); // The data was popped!
+	TEST_ASSERT_EQUAL_INT(0, mystack_nofelem(stack)); // There is no data!
+	mystack_destroy(stack);
+}
+
+void test_mystack_nofelem_InvalidStack()
+{
+	StackMeta_t *stack = NULL;
+	int result = mystack_nofelem(stack);
+	TEST_ASSERT_EQUAL_INT(-1, result);
+}
+
+void test_mystack_nofelem_Valid()
+{
+	StackMeta_t *stack = mystack_create(1);
+	int result = mystack_push(stack, "a");
+	TEST_ASSERT_EQUAL_INT(0, result);
+	TEST_ASSERT_EQUAL_INT(1, mystack_nofelem(stack));
+	mystack_destroy(stack);
+}
+
+void test_mystack_nofelem_EmptyStack()
+{
+	StackMeta_t *stack = mystack_create(1);
+	TEST_ASSERT_EQUAL_INT(0, mystack_nofelem(stack));
+	mystack_destroy(stack);
+}
+
 int main (int argc, char * argv[]) {
 	UnityBegin();
-	MY_RUN_TEST(run_my_test);
+	MY_RUN_TEST(test_myStack_Create_Valid);
+	MY_RUN_TEST(test_myStack_Create_InvalidObjectSize_Zero);
+	MY_RUN_TEST(test_myStack_push_InvalidStack);
+	MY_RUN_TEST(test_myStack_push_InvalidObject);
+	MY_RUN_TEST(test_myStack_push_Valid);
+	MY_RUN_TEST(test_myStack_pop_InvalidStack);
+	MY_RUN_TEST(test_myStack_pop_NoElements);
+	MY_RUN_TEST(test_myStack_pop_Valid);
+	MY_RUN_TEST(test_mystack_nofelem_InvalidStack);
+	MY_RUN_TEST(test_mystack_nofelem_Valid);
+	MY_RUN_TEST(test_mystack_nofelem_EmptyStack);
 	return UnityEnd();
 }
