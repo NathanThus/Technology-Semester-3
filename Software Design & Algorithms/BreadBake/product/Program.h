d2ea9dd2ef94db2df4dacccd6fd59df59eb63037
Nathan Thus <99728206+NathanThus@users.noreply.github.com>
2023-03-29 09:25:37 +0200

    Update Program.h

--- Software Design & Algorithms/BreadBake/product/Program.h
+++ Software Design & Algorithms/BreadBake/product/Program.h
@@ -11,4 +11,32 @@ struct Program
     bool addExtras;
 };
 
+//The programs
+
+Program PlainBread = { 60, 20, 160, 50, true, false };
+Program BreadPlus = { 60, 20, 160, 50, true, true };
+Program Rapid = { 0, 15, 60, 40, true, false};
+Program Dough = { 40, 20, 80, 0, true, false};
+Program Bake = {0,0,0, 69, false, false};
+
+
+//Get the program based on the ID. See the PDF for the ID's
+Program GetProgram(int id)
+{
+    switch(id)
+    {
+        case 1:
+            return PlainBread;
+        case 2:
+            return BreadPlus;
+        case 3:
+            return Rapid;
+        case 4:
+            return Dough;
+        case 5:
+            return Bake;
+    }
+    return PlainBread;
+}
+
 #endif
