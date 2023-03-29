70fb795908aa769e001c7ebda7fd5e920626bc8f
Nathan Thus <99728206+NathanThus@users.noreply.github.com>
2023-03-29 09:25:49 +0200

    Update States.h

--- Software Design & Algorithms/BreadBake/product/States.h
+++ Software Design & Algorithms/BreadBake/product/States.h
@@ -3,7 +3,15 @@
 
 enum States
 {
-    STANDBY
+    S_STANDBY,
+    S_BAKING,
+    S_KNEADING,
+    S_RISING,
+    S_RESTING,
+    S_DONE,
+    S_ERROR,
+    S_PROGRAM_SELECTION,
+    S_CANCEL
 };
 
 #endif
