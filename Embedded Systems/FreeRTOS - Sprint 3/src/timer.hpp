7ead277c3193f715397ac8484705ff6b27fca813
Nathan Thus <99728206+NathanThus@users.noreply.github.com>
2023-04-05 09:02:56 +0200

    Update timer.hpp

--- Embedded Systems/FreeRTOS - Sprint 3/src/timer.hpp
+++ Embedded Systems/FreeRTOS - Sprint 3/src/timer.hpp
@@ -13,20 +13,51 @@ enum TimerBit
     TIMER4 = 2,
 };
 
+struct BasicTimerPackage
+{
+    uint16_t prescaler;
+    uint16_t limit;
+    IRQn_Type irq;
+    TimerBit bit;
+};
+
+struct PWMTimerPackage : public BasicTimerPackage
+{
+    uint16_t pulse;
+};
+
 class Timer
 {
     public:
     Timer(TIM_TypeDef* timer);
-    void ToggleClock(TimerBit bit);
-    void DisableClock(TimerBit bit);
-    void SetPrescaler(uint16_t prescaler);
-    void SetLimit(uint16_t limit);
-    void EnableInterrupt(IRQn_Type irq);
+
+    /// @brief Enables the timer as a timer base. 
+    /// @param package The package containing the timer base settings.
+    void EnableAsTimerBase(BasicTimerPackage package);
+    /// @brief Resets the interrupt for the timer.
     void ResetInterrupt();
-    void Enable();
 
     private:
     TIM_TypeDef* timer;
+
+    /// @brief Toggles the clock for the timer.
+    /// @param bit The bit to toggle.
+    void ToggleClock(TimerBit bit);
+    /// @brief Disables the clock for the timer.
+    /// @param bit The bit to disable.
+    void DisableClock(TimerBit bit);
+    /// @brief Sets the prescaler for the timer.
+    /// @param prescaler The prescaler value.
+    void SetPrescaler(uint16_t prescaler);
+    /// @brief Sets the limit for the timer.
+    /// @param limit The limit value.
+    void SetLimit(uint16_t limit);
+    /// @brief Enables the interrupt for the timer.
+    /// @param irq The interrupt to enable.
+    void EnableInterrupt(IRQn_Type irq);
+
+    /// @brief Enables the timer.
+    void Enable();
 };
 
 #endif
