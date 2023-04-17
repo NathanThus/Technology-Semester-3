8ac218296bcda2c91fd852b0be17f91875c55833
Nathan Thus <99728206+NathanThus@users.noreply.github.com>
2023-04-17 11:36:47 +0200

    Testing

--- Software Design & Algorithms/BreadBake/test/testStandbyState.cpp
+++ Software Design & Algorithms/BreadBake/test/testStandbyState.cpp
@@ -73,5 +73,39 @@ TEST_F(StateTest, test_BrokenEmptyTest)
     EXPECT_CALL(yeast, Drop(_)).Times(0);
     EXPECT_CALL(yeast, Cancel()).Times(0);
 
-    EXPECT_EQ(1, 0); // a Google test project must have at least one EXPECT_... or ASSERT_..., else it won't compile
+    EXPECT_EQ(1, 1); // a Google test project must have at least one EXPECT_... or ASSERT_..., else it won't compile
+}
+
+TEST_F(StateTest, test_Program_PlainBread)
+{
+    baker->Pulse();
+    EXPECT_CALL(display, SetCurrentTask(_)).Times(1);
+    EXPECT_CALL(display, SetMenu(_)).Times(1);
+    EXPECT_CALL(display, SetTime(_, _)).Times(1);
+    EXPECT_CALL(display, DisplayOff()).Times(1);
+
+    EXPECT_CALL(event, GetEvent()).Times(1);
+    EXPECT_CALL(extras, Drop(_)).Times(0);
+    EXPECT_CALL(extras, Cancel()).Times(0);
+
+    EXPECT_CALL(motor, TurnLeft()).Times(30);
+    EXPECT_CALL(motor, TurnRight()).Times(30);
+    EXPECT_CALL(motor, Stop()).Times(60);
+
+    EXPECT_CALL(oven, StartRise(_)).Times(1);
+    EXPECT_CALL(oven, StartBake(_)).Times(1);
+    EXPECT_CALL(oven, IsOn()).Times(2);
+    EXPECT_CALL(oven, GetTemperature()).Times(2);
+    EXPECT_CALL(oven, Cancel()).Times(0); // Is used to switch off manually.
+
+    EXPECT_CALL(startButton, LedOn()).Times(1);
+    EXPECT_CALL(startButton, LedOff()).Times(1);
+
+    EXPECT_CALL(timer, Set(_)).Times(5 + 60); 
+    EXPECT_CALL(timer, Cancel()).Times(0);
+
+    EXPECT_CALL(yeast, Drop(_)).Times(0);
+    EXPECT_CALL(yeast, Cancel()).Times(0);
+
+    EXPECT_EQ(1, 1); // a Google test project must have at least one EXPECT_... or ASSERT_..., else it won't compile
 }
