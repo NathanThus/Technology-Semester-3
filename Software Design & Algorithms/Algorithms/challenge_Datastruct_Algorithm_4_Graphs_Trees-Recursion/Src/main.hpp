2fad1e29ebb78693dc5b12e9c97a568aa42b4974
Nathan Thus <99728206+NathanThus@users.noreply.github.com>
2023-04-24 14:24:47 +0200

    Create main.hpp

---
+++ Software Design & Algorithms/Algorithms/challenge_Datastruct_Algorithm_4_Graphs_Trees-Recursion/Src/main.hpp
@@ -0,0 +1,18 @@
+// System Includes
+
+// My Includes
+
+// My Defines
+
+#define EXPECTED_ARGC 1
+
+int main(int argc, char const *argv[])
+{
+    if(argc != EXPECTED_ARGC)
+    {
+        return -1;
+    }
+
+    // Arg = 0...
+    // Arg = 1...
+}
