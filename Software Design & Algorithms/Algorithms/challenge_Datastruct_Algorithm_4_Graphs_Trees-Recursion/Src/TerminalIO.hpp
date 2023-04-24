ccff57bbd8c0a78575e336406ac768a36561763c
Nathan Thus <99728206+NathanThus@users.noreply.github.com>
2023-04-24 14:45:13 +0200

    Update TerminalIO.hpp

--- Software Design & Algorithms/Algorithms/challenge_Datastruct_Algorithm_4_Graphs_Trees-Recursion/Src/TerminalIO.hpp
+++ Software Design & Algorithms/Algorithms/challenge_Datastruct_Algorithm_4_Graphs_Trees-Recursion/Src/TerminalIO.hpp
@@ -5,11 +5,28 @@ class TerminalIO
 private:
     /* data */
 public:
-    TerminalIO(/* args */);
+    TerminalIO();
     ~TerminalIO();
+
+    // If these are called in this order, you should read the file properly!
+    
+    /// @brief  Get the number of test cases from the file stream
+    /// @param stream The file stream to read from
+    /// @return Returns the number of test cases
     int GetNumberOfTestCases(FILE* stream);
+    /// @brief Get the number of nodes from the file stream
+    /// @param stream The file stream to read from
+    /// @return Returns the number of nodes
     int GetNumberOfNodes(FILE* stream);
+
+    /// @brief Get the number of connections from the file stream
+    /// @param stream The file stream to read from
+    /// @return Returns the number of connections
     int GetNumberOfConnections(FILE* stream);
     
+    /// @brief Prints the result to the file stream
+    /// @param stream The file stream to write to
+    /// @param result The result (array of integers)
+    /// @param numberOfTestCases The number of test cases
     void PrintResult(FILE* stream, int *result, int numberOfTestCases);
 };
