5931d5ae2548774edfc6a1dac4611429a93a96e1
Nathan Thus <99728206+NathanThus@users.noreply.github.com>
2023-05-25 13:32:00 +0200

    Implemented Argument Handling

--- Software Design & Algorithms/Algorithms/challenge_Datastruct_Algorithm_4_Graphs_Trees-Recursion/Src/main.cpp
+++ Software Design & Algorithms/Algorithms/challenge_Datastruct_Algorithm_4_Graphs_Trees-Recursion/Src/main.cpp
@@ -1,25 +1,74 @@
 // System Includes
-
 // My Includes
+#include "TerminalIO.hpp"
+#include "Tree.hpp"
 
 // My Defines
+#define REQUESTED_TEST 1
+#define TREE "TREE"
+#define RECURSION "RECURSION"
 
-#define EXPECTED_ARGC 1
+TerminalIO terminalIO;
+
+int HandleTree()
+{
+    int testCases = 0;
+    if(terminalIO.GetNumberOfTestCases(&testCases) == -1)
+    {
+        terminalIO.PrintText("Invalid number of test cases");
+        return -1;
+    }
+    int goal = 0;
+    int connections = 0;
+    if(terminalIO.GetTestParameters(&goal, &connections) == -1)
+    {
+        terminalIO.PrintText("Invalid number of nodes or connections");
+        return -1;
+    }
+
+    int source = 0;
+    int destination = 0;
+    if(terminalIO.GetConnection(&source, &destination, goal) == -1)
+    {
+        terminalIO.PrintText("Invalid connection");
+        return -1;
+    }
+
+    Tree tree;
+    tree.GenerateChildren(goal);
+
+    for (int i = 0; i < connections; i++)
+    {
+        int sourceLeafID = 0, destinationLeafID = 0;
+        terminalIO.GetConnection(&sourceLeafID, &destinationLeafID, goal);
+        tree.AddConnection(sourceLeafID, destinationLeafID);
+    }
+    
+
+    return 0;
+}
+
+int HandleRecursion()
+{
+    terminalIO.PrintText("Not implemented");
+    return -1; // NOT IMPLEMENTED
+}
 
 int main(int argc, char const *argv[])
 {
-    if(argc != EXPECTED_ARGC)
+    if(argc < REQUESTED_TEST)
     {
+        terminalIO.PrintText("Not enough arguments");
         return -1;
     }
 
-    if(argv == nullptr)
+    if(strcmp(argv[REQUESTED_TEST], TREE) == 0)
     {
-        return -1;
+        return HandleTree();
     }
-
-    // Arg = 0...
-    // Arg = 1...
-
+    if(strcmp(argv[REQUESTED_TEST], RECURSION) == 0)
+    {
+        return HandleRecursion();
+    }
     return 0;
 }
