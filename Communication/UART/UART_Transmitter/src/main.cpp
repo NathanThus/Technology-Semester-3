d1425d5f7f6a7116aa16c8b513da860f6dc39286
Nathan Thus <99728206+NathanThus@users.noreply.github.com>
2023-06-09 13:14:09 +0200

    Transmitter working at 9600

--- Communication/UART/UART_Transmitter/src/main.cpp
+++ Communication/UART/UART_Transmitter/src/main.cpp
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 
-#define BAUDRATE 300
+#define BAUDRATE 9600
 #define STARTBIT 0
 #define STOPBIT 1
 
@@ -17,54 +17,75 @@ constexpr int numberOfBits = numberOfDataBits + numberOfStopBits + numberOfParit
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(BAUDRATE);
+  pinMode(13, OUTPUT);
+  pinMode(2, OUTPUT);
 }
 
 void loop() {
-  if(!Serial.available())
-  {
-    return;
-  }
+  // if(!Serial.available())
+  // {
+  //   return;
+  // }
 
-  char inboundByte = Serial.read();
-  int bitCount = 0;
+  // char inboundByte = Serial.read();
 
-  int exportByte[numberOfBits];
-  int partiyBit = 0;
+  int exportByte[numberOfBits] = {0};
+  // int partiyBit = 0;
 
-  for (int i = 0; i < numberOfStartBits; i++)
-  {
-    exportByte[bitCount] = STARTBIT;
-    bitCount++;
-  }
+  // exportByte[0] = STARTBIT;
   
-  // Insert data
-  for (int i = numberOfStartBits; i < numberOfDataBits; i++)
-  {
-    exportByte[bitCount] = (inboundByte >> i) & 1;
-    partiyBit += exportByte[bitCount];
-    bitCount++;
-  }
+  // Insert data //TODO: Fix
+  // for (int i = numberOfStartBits; i < numberOfDataBits; i++)
+  // {
+  //   exportByte[i] = (inboundByte >> (i - 1)) & 1; //TODO: Might not be optimal.
+  // }
 
-  // // Insert parity
+  //Fill exportByte with the data of 'a'
+  exportByte[0] = 0;
+  exportByte[1] = 1;
+  exportByte[2] = 0;
+  exportByte[3] = 0;
+  exportByte[4] = 0;
+  exportByte[5] = 0;
+  exportByte[6] = 1;
+  exportByte[7] = 1;
+  exportByte[8] = 0;
+  exportByte[9] = 1;
+
+  // Insert parity
   // for (int i = numberOfDataBits; i < numberOfParityBits; i++)
   // {
-  //   exportByte[bitCount] = partiyBit % 2; // TODO: Check if this is correct
-  //   bitCount++;
+  //   exportByte[i] = partiyBit % 2; // TODO: Check if this is correct
+  //   i++;
   // }
   
-  for (int i = numberOfDataBits + partiyBit; i < numberOfStopBits; i++)
-  {
-    exportByte[bitCount] = STOPBIT;
-    bitCount++;
-  }
-  
+  // for (int i = numberOfDataBits + partiyBit; i < numberOfStopBits; i++)
+  // {
+  //   exportByte[i] = STOPBIT;
+  // }
 
-  bitCount = 0;
-//TRANSMITTING
-  while(bitCount < numberOfBits)
-  {
+  // int bitcount = 0;
+  unsigned long nextBitTime = micros();
+  //OLD
+  // while (bitcount < numberOfBits)
+  // {
+  //   if (micros() >= nextBitTime)
+  //   {
+  //     nextBitTime = micros() + TimePerBit;
+  //     digitalWrite(2, exportByte[bitcount]);
+  //     ++bitcount;
+  //   }
+  // }
 
-    bitCount++;
+  for (int i = 0; i < numberOfBits; i++)
+  {
+    // digitalWrite(13,!digitalRead(13));
+    while(micros() < nextBitTime)
+    {
+    }
+    nextBitTime = micros() + TimePerBit;
+    digitalWrite(2, exportByte[i]);
   }
+  delay(100);
 }
 
